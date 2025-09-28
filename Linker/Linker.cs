0f8d0c1797e7213646146679c97f4e82b32774ec
MyEngine-Official <myengine.official@outlook.com>
2025-09-27 21:14:38 -0400

    primera version

--- Linker/Linker.cs
+++ Linker/Linker.cs
@@ -5,69 +5,80 @@ namespace MyLinker.Linker
 {
     public class Linker<EN, EX>
     {
-        public static EX MapObjects(EN entryObject, EX exitObject, bool nullPropertySafety, string[]? propertyOmit) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, nullPropertySafety, propertyOmit);
-        public static EX MapObjects(EN entryObject, EX exitObject, bool nullPropertySafety) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, nullPropertySafety, null);
-        public static EX MapObjects(EN entryObject, EX exitObject, string[] propertyOmit) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, propertyOmit);
-        public static EX MapObjects(EN entryObject, EX exitObject, string propertyOmit) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, [propertyOmit]);
-        public static EX MapObjects(EN entryObject, EX exitObject, string propertyOmit1, string propertyOmit2) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, [propertyOmit1, propertyOmit2]);
-        public static EX MapObjects(EN entryObject, EX exitObject, string propertyOmit1, string propertyOmit2, string propertyOmit3) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, [propertyOmit1, propertyOmit2, propertyOmit3]);
-        public static EX MapObjects(EN entryObject, EX exitObject, string propertyOmit1, string propertyOmit2, string propertyOmit3, string propertyOmit4) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, [propertyOmit1, propertyOmit2, propertyOmit3, propertyOmit4]);
-        public static EX MapObjects(EN entryObject, EX exitObject, string propertyOmit1, string propertyOmit2, string propertyOmit3, string propertyOmit4, string propertyOmit5) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, [propertyOmit1, propertyOmit2, propertyOmit3, propertyOmit4, propertyOmit5]);
-        public static EX MapObjects(EN entryObject, EX exitObject) => Linker<EN, EX>.BaseMapObjects(entryObject, exitObject, null, null);
-        private static EX BaseMapObjects(EN entryObject, EX exitObject, bool? nullPropertySafety, string[]? propertyOmit)
+        public static EX MapObjects(EN entryObject, EX exitObject, bool nullPropertySafety = true, params string[]? propertyOmit) => BaseMapObjects(entryObject, exitObject, nullPropertySafety, propertyOmit);
+        private static EX BaseMapObjects(EN entryObject, EX exitObject, bool nullPropertySafety, string[]? propertyOmit = null)
         {
+            if (entryObject == null) throw new ArgumentNullException(nameof(entryObject));
+            if (exitObject == null) throw new ArgumentNullException(nameof(exitObject));
+
             try
             {
                 PropertyInfo[] entryProperties = typeof(EN).GetProperties();
+                var exitProperties = exitObject?.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public);
 
                 foreach (PropertyInfo property in entryProperties)
                 {
-                    var TarjetProperty = exitObject?.GetType().GetProperties(BindingFlags.Instance | BindingFlags.Public).FirstOrDefault(x => x.Name.ToLower() == property.Name.ToLower());
-                    if (TarjetProperty == null) continue;
-                    if (propertyOmit != null && propertyOmit.Contains(TarjetProperty.Name)) continue;
-                    if (property.GetValue(entryObject) == null || TarjetProperty.GetValue(exitObject) == null) continue;
+                    var TargetProperty = exitProperties!.FirstOrDefault(x => x.Name.Equals(property.Name, StringComparison.OrdinalIgnoreCase));
+                    if (TargetProperty == null) continue;
 
-                    if (TarjetProperty.PropertyType == property.PropertyType)
-                    {
-                        TarjetProperty.SetValue(exitObject, property.GetValue(entryObject));
+                    if (property.GetValue(entryObject) == null && nullPropertySafety == true) continue;
+                    if (propertyOmit != null && propertyOmit.Contains(TargetProperty.Name)) continue;
 
-                    }else if (TarjetProperty.PropertyType == typeof(string))
+                    if (TargetProperty.PropertyType == property.PropertyType)
                     {
-                        TarjetProperty.SetValue(exitObject, property?.GetValue(entryObject)?.ToString());
-                    }else if(TarjetProperty.PropertyType == typeof(bool) && property.PropertyType == typeof(string))
+                        TargetProperty.SetValue(exitObject, property.GetValue(entryObject));
+
+                    }else if (TargetProperty.PropertyType == typeof(string))
                     {
-                        TarjetProperty.SetValue(exitObject, bool.Parse((string)property?.GetValue(entryObject)!));
+                        TargetProperty.SetValue(exitObject, property.GetValue(entryObject)!.ToString());
+                    }else if(TargetProperty.PropertyType == typeof(bool) && property.PropertyType == typeof(string))
+                    {
+                        if (bool.TryParse((string)property.GetValue(entryObject), out bool boolExit)){
+                            TargetProperty.SetValue(exitObject, boolExit);
+                        }
                     }
-                    else if(TarjetProperty.PropertyType == typeof(bool) && property.PropertyType == typeof(int))
+                    else if(TargetProperty.PropertyType == typeof(bool) && property.PropertyType == typeof(int))
                     {
-                        if((int)property.GetValue(entryObject) == 1)
+                        if((int)property.GetValue(entryObject)! == 1)
                         {
-                            TarjetProperty.SetValue(exitObject, true);
+                            TargetProperty.SetValue(exitObject, true);
                         }
-                        else if((int)property.GetValue(entryObject) == 0)
+                        else if((int)property.GetValue(entryObject)! == 0)
                         {
-                            TarjetProperty.SetValue(exitObject, false);
+                            TargetProperty.SetValue(exitObject, false);
                         }
-                    }else if (TarjetProperty.PropertyType == typeof(int))
+                    }else if (TargetProperty.PropertyType == typeof(int))
                     {
                         int valor;
                         if (property.PropertyType == typeof(string))
                         {
-                            _ = int.TryParse((string)property.GetValue(entryObject)!, out int result);
-                            valor = (int)result;
+                            if (int.TryParse((string)property.GetValue(entryObject)!, out int result))
+                            {
+                                valor = result;
+                            }
+                            else
+                            {
+                                continue;
+                            }
                         }
                         else if(property.PropertyType != typeof(bool))
                         {
-                            valor = (int)(property.GetValue(entryObject));
+                            valor = Convert.ToInt32(property.GetValue(entryObject));
                         }
                         else
                         {
-                            if((bool)property.GetValue(entryObject)! == true)
-                            valor = 1;
+                            if ((bool)property.GetValue(entryObject)! == true)
+                            {
+                                valor = 1;
+                            }
+                            else
+                            {
+                                valor = 0;
+                            }
 
-                            valor = 0;
+                            
                         }
-                            TarjetProperty.SetValue(exitObject, valor);
+                            TargetProperty.SetValue(exitObject, valor);
                     }
                 }
 
